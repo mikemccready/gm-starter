5092d7a3f6112d0df5d72fcd1960c823adfd1251
mikemccready <michaeljmccready@gmail.com>
2023-12-26 23:02:21 -0800

    🦄 Set `wagmi` config

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WagmiConfig, createConfig } from 'wagmi'
+import { createPublicClient, http } from 'viem'
+import { hardhat } from 'viem/chains'
+
+const config = createConfig({
+	autoConnect: true,
+	publicClient: createPublicClient({
+		chain: hardhat,
+		transport: http(),
+	}),
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<WagmiConfig config={config}>
+			<Component {...pageProps} />
+		</WagmiConfig>
+	)
 }
